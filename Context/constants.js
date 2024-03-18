d188d7cb25c00e7750192157b04eb4d5335dca77
Casper <37352901+Casper0513@users.noreply.github.com>
2024-03-18 15:47:59 -0600

    Update constants.js

--- Context/constants.js
+++ Context/constants.js
@@ -1,13 +1,13 @@
-//GemoneycoinToken = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-//TokenSale: = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+//GemoneycoinToken = "0xA952422204810B1A5C9C5a64e006F95Bb9FD1FC3"
+//TokenSale: = "0xe1bdb7ce67f16a9f0e9fd83e52f37fdb5ed8e897"
 
 import GemoneycoinToken from "./GemoneycoinToken.json";
 import TokenSale from "./TokenSale.json";
 
 //TOKEN
-export const TOKEN_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const TOKEN_ADDRESS = "0xA952422204810B1A5C9C5a64e006F95Bb9FD1FC3";
 export const TOKEN_ABI = GemoneycoinToken.abi;
 
 //TOKEN SALE
-export const TOKEN_SALE_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const TOKEN_SALE_ADDRESS = "0xe1bdb7ce67f16a9f0e9fd83e52f37fdb5ed8e897";
 export const TOKEN_SALE_ABI = TokenSale.abi;
